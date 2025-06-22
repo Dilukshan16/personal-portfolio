6aee70fb842e859f245c3f29e6e02410626fc005
Dilukshan16 <pdiluksan@gmail.com>
2025-06-23 00:37:31 +0530

    completed hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,11 +6,26 @@ const Hero = () => {
   return (
     <div className="hero">
       <img
+        className="profile"
         src="https://i.postimg.cc/y6DvrT5N/Whats-App-Image-2025-04-04-at-15-55-52-9b71d0b9.jpg"
         alt=""
+        style={{
+          width: "250px",
+          height: "auto",
+          borderRadius: "300px",
+        }}
       />
-      <h1>I'm Pushparajah Dilukshan</h1>
-      <h2>Undergraduate Software Developer</h2>
+      <h1>
+        {" "}
+        <span>I'm Dilukshan </span>| Undergraduate Software Developer
+      </h1>
+      <p>
+        I’m an undergraduate Software Engineering student with a strong interest
+        in full-stack development, mobile apps, and cloud technologies. I have
+        hands-on experience with React, Flutter, Firebase, and Java, and I’m
+        constantly learning to improve my development skills and build impactful
+        digital solutions.
+      </p>
       <div className="hero-action">
         <div className="hero-connect">Connect With Me</div>
         <div className="hero-resume">My Resume</div>
