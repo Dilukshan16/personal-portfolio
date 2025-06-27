3f610d8dc3ae06e5284cd1fc6de0d15776a88919
Dilukshan16 <pdiluksan@gmail.com>
2025-06-27 09:44:21 +0530

    navbar comp changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./components/NavBar/NavBar";
+import Navbar from "./components/NavBar/Navbar";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 import Services from "./components/Services/Services";
